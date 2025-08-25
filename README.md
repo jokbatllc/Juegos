a46a635709ff5bb2399e132803feed28bf315487
jokbatllc <jokbatllc@gmail.com>
2025-08-25 13:22:05 +0200

    FASE 0: apps específicas creadas y enrutadas

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Juegos
 Proyecto Django para crucigramas, sopas de letras, etc.
+
+## Instalación
+
+1. Crear y activar un entorno virtual:
+   `python -m venv venv && source venv/bin/activate`
+2. Instalar dependencias:
+   `pip install -r requirements.txt`
+3. Ejecutar las migraciones:
+   `python manage.py migrate`
+4. Crear un superusuario:
+   `python manage.py createsuperuser`
+5. Iniciar el servidor de desarrollo:
+   `python manage.py runserver`
---
+++ juegos/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('lexicon/', include('lexicon.urls', namespace='lexicon')),
+    path('puzzles/', include('puzzles.urls', namespace='puzzles')),
+    path('crossword/', include('crossword.urls', namespace='crossword')),
+    path('wordsearch/', include('wordsearch.urls', namespace='wordsearch')),
+    path('coloring/', include('coloring.urls', namespace='coloring')),
+    path('calligraphy/', include('calligraphy.urls', namespace='calligraphy')),
+    path('sudoku/', include('sudoku.urls', namespace='sudoku')),
+    path('mandala/', include('mandala.urls', namespace='mandala')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
